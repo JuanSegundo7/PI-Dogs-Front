4b34dd852df14cb62be74a0478c711a2e8c4dc37
Juan Segundo <juansepincha@gmail.com>
2022-12-04 19:48:19 -0300

    updating front

--- src/Redux/Actions/Actions.js
+++ src/Redux/Actions/Actions.js
@@ -16,6 +16,7 @@ export const ADD_FAVORITE = "ADD_FAVORITE";
 export const COPY_DOGS = "COPY_DOGS";
 export const DELETE_DOG = "DELETE_DOG";
 
+//hola
 // const baseUrl = "http://localhost:3001/dogs"
 // const baseUrlTemps = "http://localhost:3001/temperaments"
 const baseUrl = "https://pi-dogs-back-production-9a83.up.railway.app/dogs"
